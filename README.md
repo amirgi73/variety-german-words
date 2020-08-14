afc954301791ac14899c81fa38d2064753d8bcb6
Amir Ganji <37738371+amirgi73@users.noreply.github.com>
2020-08-14 20:06:44 +0430

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# variety-german-words
+
+Plugin for Variety wallpaper changer 
+
+A quotes plugin to show German words with translation and examples. A helpful way to get better at German
+
+## Installation
+
+- Copy the 'german_words.py' file into ~/.config/variety/plugins.
+- Restart variety.
+- Activate the plugin from Variety Preferences > Effects > Quotes > Sources and filtering.
+
+## Requirements
+
+Needs
+- Variety - https://peterlevi.com/variety/
+
+## Notes
+
+- This plugin was tested with Variety 0.8.3 (python 3.8)
+- This plugin is in early development.
+
+## Credits
+
+Variety is developed by Peter Levi  - https://peterlevi.com/variety/
+
+german_words.py is inspired by variety-goodreads-quotes  - https://github.com/dsbmac/variety-goodreads-quotes and variety-vdm-quotes  - https://github.com/LG666/variety-vdm-quotes
+
+
+### License
+
+MIT - Copyright(c) 2020 amirgi73
---
+++ german_words.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+### BEGIN LICENSE
+# Copyright (c) 2020, amirgi73
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+### END OF LICENSE
+
+"""
+    A quotes plugin to show German words with translation and examples.
+    A helpful way to get better at German
+    
+    This script is placed in '~/.config/variety/plugins' and then activated
+    from inside Variety's  Preferences Quotes menu
+"""
+
+import logging
+import random
+from locale import gettext as _
+from csv import reader
+from variety.Util import Util
+from variety.plugins.IQuoteSource import IQuoteSource
+import os
+
+logger = logging.getLogger("variety")
+
+german_words_file = os.path.expanduser('~/.config/variety/plugins/german_words/words.csv')
+
+
+class GermanWordsSource(IQuoteSource):
+    """
+        Retriving words and their translations from words.csv file
+        Attributes:
+            quotes(list): list containing the quotes
+    """
+
+    def __init__(self):
+        self.active = False
+        super(IQuoteSource, self).__init__()
+        self.quotes = []
+
+    @classmethod
+    def get_info(cls):
+        return {
+            "name": "German Words",
+            "description": _("Showing German words and their meaning as a way to learn German\n"
+                             "Does not support searching by tags or authors."),
+            "author": "amirgi73",
+            "url": "https://github.com/amirgi73/variety-german-words",
+            "version": "0.0.1"
+        }
+
+    def supports_search(self):
+        return False
+
+    def activate(self):
+        if self.active:
+            return
+        self.active = True
+        self.quotes = []
+        self.fetch_german_words()
+
+    def deactivate(self):
+        self.quotes = []
+        self.active = False
+
+    # def is_active(self):
+    #    return self.active
+
+    def fetch_german_words(self):
+        self.quotes = []
+        # TODO: read the german_words.csv and return them
+        with open(german_words_file, 'r') as fin:
+            file_reader = reader(fin)
+            words_from_file = []
+            for row in file_reader:
+                data = {
+                    'English': row[1],
+                    'Persian G-translate': row[2],
+                    'Persian B-amooz': row[3],
+                    'German': row[5],
+                    'German Alternatives': row[6],
+                    'Level': row[7],
+                    'Part of Speech': row[8],
+                    'Plural and Inflected Forms': row[10],
+                    'Sample Sentence': row[11],
+                }
+                words_from_file.append(data)
+
+        for item in words_from_file:
+            if item.get('Plural and Inflected Forms'):
+                source_name = f"\tPlural/Inflected Forms: {item.get('Plural and Inflected Forms')}"
+            else:
+                source_name = ''
+            self.quotes.append({
+                "quote": f"{item.get('German')} : {item.get('English')}\n{item.get('Sample Sentence')}\n",
+                "author": item.get('Part of Speech'),
+                "sourceName": source_name})
+
+        if not self.quotes:
+            logger.warning("Could not read words ")
+
+    def get_for_author(self, author):
+        return []
+
+    def get_for_keyword(self, keyword):
+        return []
+
+    def get_random(self):
+        return self.quotes
